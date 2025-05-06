5ba422a356958ed2f38261209b055b3377d3071a
Liam Pattinson <liampattinson@gmail.com>
2025-05-06 09:56:01 +0100

    Highlight current page on topbar, improve spacing

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,9 +1,46 @@
 import { Description as CV, GitHub, Home } from '@mui/icons-material';
-import { AppBar, Box, IconButton, Link, Toolbar } from '@mui/material';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import DarkModeToggle from './DarkMode.jsx';
 
 export default function TopBar({ darkMode, setDarkMode }) {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  useEffect(() => {
+    console.log('Location changed:', location.pathname);
+    setCurrentPath(location.pathname);
+  }, [location]);
+
+  const boxShadow = (paths) =>
+    paths.some((x) => x === currentPath)
+      ? '0px 4px 10px rgba(0, 0, 0, 0.2)'
+      : null;
+
+  const highlight = (paths) =>
+    paths.some((x) => x === currentPath) ? 'rgb(20, 146, 196)' : null;
+
+  const TopBarIconButton = ({ href, alts, children }) => {
+    alts = alts ?? [];
+    let paths = [...alts, href];
+    href = href.includes('/#') ? href : `/#${href}`;
+    return (
+      <IconButton
+        href={href}
+        sx={{
+          ml: { xs: 1, md: 2 },
+          mr: { xs: 1, md: 2 },
+          color: highlight(paths),
+          boxShadow: boxShadow(paths),
+        }}
+      >
+        {children}
+      </IconButton>
+    );
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -23,12 +60,12 @@ export default function TopBar({ darkMode, setDarkMode }) {
           }}
         >
           <Box>
-            <IconButton href="/#/home">
+            <TopBarIconButton href="/home" alts={['/']}>
               <Home />
-            </IconButton>
-            <IconButton href="/#/cv">
+            </TopBarIconButton>
+            <TopBarIconButton href="/cv">
               <CV />
-            </IconButton>
+            </TopBarIconButton>
           </Box>
           <Box>
             <IconButton href="https://www.github.com/LiamPattinson">
