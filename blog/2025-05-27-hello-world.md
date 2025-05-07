aa3884e7a079a215c5989c7700d2abff61f65a6b
Liam Pattinson <liampattinson@gmail.com>
2025-05-07 23:28:11 +0100

    Add script turning blog markdown to json

---
+++ blog/2025-05-27-hello-world.md
@@ -0,0 +1,17 @@
+---
+title: Hello, World!
+subtitle: Making a personal website with React.
+published: May 7th 2025
+---
+
+The quick brown fox jumped over the lazy dog. The quick brown fox jumped over
+the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox
+jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The
+quick brown fox jumped over the lazy dog. The quick brown fox jumped over the
+lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox
+jumped over the lazy dog. The quick brown fox jumped over the lazy dog.
+
+```python
+import numpy as np
+print("hello world")
+```
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,73 +1,87 @@
-import matter from 'gray-matter';
+import { Box, Card, Divider, Typography } from '@mui/material';
 import Markdown from 'react-markdown';
-import Prism from 'react-syntax-highlighter';
+import { Prism } from 'react-syntax-highlighter';
 import { xonokai } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-import { BlogEntry } from './Blog/components';
-
-const markdown = `---
-title: Hello World
-subtitle: My first blog post
-published: May 7th 2025
----
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-mollit anim id est laborum.
-
-~~~python
-import numpy as np
-
-print("hello world")
-
-with syntax_highlighting as yay:
-   print("woo")
-~~~
-`;
+import { BaseBox, Heading } from '../core_components';
+import articles from './Blog/articles.jsx';
 
 // TODO:
 // - Each blog post should be its own page under ./Blog
 // - The front page should be a list of all the blog posts.
 // - The blog post URL should be /blog/yyyymmdd-title
-// - Provide some function to automate the addition of new posts.
 // - Each blog post should have a proper date
 // - Provide prev/next navigation buttons at the bottom of each page.
 
-function Blog() {
-  let md = matter(markdown);
-  return (
-    <BlogEntry
-      title={md.data.title}
-      subtitle={md.data.subtitle}
-      published={md.data.published}
+function BlogEntry({ title, subtitle, published, children }) {
+  let heading = (
+    <Box
+      sx={{
+        p: 2,
+        width: '100%',
+        textAlign: 'justify',
+        display: 'flex',
+        flexShrink: 1,
+        flexDirection: 'column',
+      }}
     >
-      <Markdown
-        children={md.content}
-        components={{
-          code(props) {
-            const { children, className, node, ...rest } = props; // eslint-disable-line no-unused-vars
-            const match = /language-(\w+)/.exec(className || '');
-            return match ? (
-              <Prism
-                {...rest}
-                PreTag="div"
-                children={String(children).replace(/\n$/, '')}
-                language={match[1]}
-                style={xonokai}
-              />
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      />
-    </BlogEntry>
+      <Heading>{title}</Heading>
+      <Typography variant="h6" sx={{ fontWeight: 'light', mb: 2 }}>
+        {subtitle}
+      </Typography>
+      <Typography variant="caption" sx={{ fontWeight: 'light', mb: 1 }}>
+        Published: {published}
+      </Typography>
+    </Box>
+  );
+  let article = (
+    <BaseBox>
+      <Card variant="outlined" sx={{ maxWidth: '100%' }}>
+        {heading}
+        <Divider variant="middle" flexItem sx={{ mb: 4 }} aria-hidden="true" />
+        <Box
+          sx={{
+            p: 2,
+            width: '100%',
+            textAlign: 'justify',
+            display: 'flex',
+            flexShrink: 1,
+            flexDirection: 'column',
+          }}
+        >
+          {children}
+        </Box>
+      </Card>
+    </BaseBox>
+  );
+  return article;
+}
+
+function highlight_syntax(props) {
+  const { children, className } = props;
+  const match = /language-(\w+)/.exec(className || '');
+  return match ? (
+    <Prism language={match[1]} style={xonokai}>
+      {children}
+    </Prism>
+  ) : (
+    <code className={className}>{children}</code>
+  );
+}
+
+function Blog() {
+  return (
+    <>
+      {articles.map((md, idx) => (
+        <span key={idx}>
+          <BlogEntry key={idx} {...md.data}>
+            <Markdown components={{ code: highlight_syntax }}>
+              {md.content}
+            </Markdown>
+          </BlogEntry>
+        </span>
+      ))}
+    </>
   );
 }
 
