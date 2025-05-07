50140f02a41280ce6a384fa486119952efe30981
Liam Pattinson <liampattinson@gmail.com>
2025-05-07 19:45:17 +0100

    Add markdown parsing
    Syntax highlighting doesn't seem to be working.
    
    It might have worked before adding gray-matter, double check that

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,33 @@
-import { Paragraph } from '../core_components';
+import matter from 'gray-matter';
+import Markdown from 'react-markdown';
+import Prism from 'react-syntax-highlighter';
+import { xonokai } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 import { BlogEntry } from './Blog/components';
+
+const markdown = `---
+title: Hello World
+subtitle: My first blog post
+published: May 7th 2025
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum.
+
+~~~python
+import numpy as np
+
+print("hello world")
+
+with syntax_highlighting as yay:
+   print("woo")
+~~~
+`;
 
 // TODO:
 // - Each blog post should be its own page under ./Blog
@@ -10,21 +38,35 @@ import { BlogEntry } from './Blog/components';
 // - Provide prev/next navigation buttons at the bottom of each page.
 
 function Blog() {
+  let md = matter(markdown);
   return (
     <BlogEntry
-      title="Hello World"
-      subtitle="My first blog post"
-      published="May 7th 2025"
+      title={md.data.title}
+      subtitle={md.data.subtitle}
+      published={md.data.published}
     >
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Paragraph>
+      <Markdown
+        children={md.content}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props; // eslint-disable-line no-unused-vars
+            const match = /language-(\w+)/.exec(className || '');
+            return match ? (
+              <Prism
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={xonokai}
+              />
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
     </BlogEntry>
   );
 }
